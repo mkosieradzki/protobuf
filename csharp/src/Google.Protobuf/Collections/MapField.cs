4e251e769bb5e196bfbf74210c534121468fab4a
Marcin Kosieradzki <m.kosieradzki@p2ware.com>
2017-06-02 14:13:40 +0200

    Removed Tasks remainders from MapField

--- csharp/src/Google.Protobuf/Collections/MapField.cs
+++ csharp/src/Google.Protobuf/Collections/MapField.cs
@@ -37,8 +37,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Google.Protobuf.Collections
 {
