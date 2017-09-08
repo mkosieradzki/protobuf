2e87f804d10ce3fe44b72da289e75c0a712b3712
Marcin Kosieradzki <m.kosieradzki@p2ware.com>
2017-09-08 11:47:35 +0200

    Updated code

--- csharp/src/Google.ProtobufBenchmark/Program.cs
+++ csharp/src/Google.ProtobufBenchmark/Program.cs
@@ -26,5 +26,41 @@ namespace Google.ProtobufBenchmark
             sw.Stop();
             Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds}ms GC0={GC.CollectionCount(0)} GC1={GC.CollectionCount(1)} GC2={GC.CollectionCount(2)} PeakMem={Process.GetCurrentProcess().PeakWorkingSet64}");
         }
+
+        static void CreateTestFile()
+        {
+            var addressBook = new AddressBook();
+            for (int i = 0; i < 1000; i++)
+            {
+                addressBook.People.Add(new Person
+                {
+                    Id = i,
+                    Email = $"person{i}@contoso.com",
+                    Name = $"Person Named {i}",
+                    Phones =
+                    {
+                        new Person.Types.PhoneNumber
+                        {
+                            Type = Person.Types.PhoneType.Home,
+                            Number = $"1234567{i}"
+                        },
+                        new Person.Types.PhoneNumber
+                        {
+                            Type = Person.Types.PhoneType.Mobile,
+                            Number = $"001234567{i}"
+                        },
+                        new Person.Types.PhoneNumber
+                        {
+                            Type = Person.Types.PhoneType.Work,
+                            Number = $"234567{i}"
+                        }
+                    }
+                });
+            }
+
+            using (var stream = File.Create(@"C:\protobench\addressbook1.bin"))
+            using (var codedOutputStream = new Protobuf.CodedOutputStream(stream))
+                addressBook.WriteTo(codedOutputStream);
+        }
     }
 }
